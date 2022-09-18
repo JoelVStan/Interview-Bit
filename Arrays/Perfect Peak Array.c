00728d37a49198d5c65bac0385e9726b41861d8f
Joel V Stanley <101164781+JoelVStan@users.noreply.github.com>
2022-09-18 10:59:19 +0530

    Update Perfect Peak Array.c

--- Arrays/Perfect Peak Array.c
+++ Arrays/Perfect Peak Array.c
@@ -25,3 +25,37 @@ int Solution::perfectPeak(vector<int> &A) {
     return flag;
     
 }
+
+// This is the best approach
+
+int Solution::perfectPeak(vector<int> &A) {
+    int i;
+    int n = A.size();
+    int mx[n], mn[n];
+    
+    mx[0] = A[0];
+    mn[n-1] = A[n-1];
+    
+    int mxx = A[0], mnn = A[n-1];
+    
+    for (i=1; i<n; i++)
+    {
+        mxx = max(mxx, A[i]);
+        mx[i] = mxx;
+    }
+    
+    for (i=n-2; i>=0; i--)
+    {
+        mnn = min(mnn, A[i]);
+        mn[i] = mnn;
+    }
+    
+    for(int i=1; i<n-1; i++)
+    {
+      if(mn[i]==mx[i] && mn[i]!=mn[i+1]&& mx[i-1]!=mx[i] && mx[i]==A[i])
+      {
+        return 1;
+      }
+    }
+    return 0;
+}
